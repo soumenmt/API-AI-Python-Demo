ff06ea34b1f06bfef5e6781fa3204ab7b4ca5e64
soumenmt <soumenmt@gmail.com>
2017-06-23 01:39:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,17 @@
-from urllib.parse import urlparse, urlencode
-from urllib.request import urlopen, Request
-from urllib.error import HTTPError
-
+import urllib
 import json
 import os
 
 from flask import Flask
 from flask import request
 from flask import make_response
-import requests
+
 # Flask app should start in global layout
 app = Flask(__name__)
-@app.route('/webhook')
-def index():
-	r = requests.get('http://192.168.66.128:9080/prweb/PRRestService/comm/commdata/GetServiceStatus')
-	c = r.content
-	j = json.loads(c)
-	a= j['ONTStatus']+j['RGStatus']+j['STBStatus']
-	return a
+
+
+@app.route('/')
+def root():
+    return "Hello World"
 if __name__ == '__main__':
     app.run(debug=True)
