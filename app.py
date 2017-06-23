3f2ef9ee44493bbbe4780c788287210853557a52
soumenmt <soumenmt@gmail.com>
2017-06-23 01:45:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -14,4 +14,8 @@ app = Flask(__name__)
 def root():
     return "Hello World"
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.getenv('PORT', 5000))
+
+    print "Starting app on port %d" % port
+
+    app.run(debug=False, port=port, host='0.0.0.0')
